6a5cf7f6656916b0557752b13c6707c2fdc2d914
JGilbe28-7 <jonathan.gilbert@mac.com>
2020-12-12 21:37:49 -0500

    updated server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// needed to connect to the public folder
 app.use(express.static("public"));
 
 require("./routes/apiRoutes")(app);
