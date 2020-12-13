f96a1018ec3fa780b804e3d94e73c4fc054e7d7d
JGilbe28-7 <jonathan.gilbert@mac.com>
2020-12-12 21:22:20 -0500

    updated the Server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
-const htmlRoute = require("/routes/htmlRoutes")
-const apiRoute = require("/routes/apiRoutes")
+
+
 
 // Sets up the Express App used the Starwars and HotResturant as example
 const app = express();
@@ -10,9 +11,10 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use("/", htmlRoute);
-app.use("/api", apiRoute)
+app.use(express.static(__dirname));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // the Listener to start the server
 app.listen(PORT, function() {
